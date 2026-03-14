84fc0cce29bc9caef721b4d4776432ba4007421a
caser1122115 <1729541778@qq.com>
2026-03-14 13:43:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#
-# File name: diy-part1.sh
-#
 
+# 仅保留 PassWall 官方源，它与 ImmortalWrt 兼容性最好
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-sed -i 's/^src-git helloworld/#src-git helloworld/' feeds.conf.default
+# 如果你需要更多插件，可以加 kenzo，但绝对不要再加 small 或者 helloworld
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
