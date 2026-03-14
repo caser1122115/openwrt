641206bb00354d2c7ee5586f70a0e152a4d4f97f
caser1122115 <1729541778@qq.com>
2026-03-14 13:30:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# 
-# sed -i 's/^src-git helloworld/#src-git helloworld/' feeds.conf.default
+
+sed -i 's/^src-git helloworld/#src-git helloworld/' feeds.conf.default
