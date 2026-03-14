79902c72d5f02750f1386d967984c05eceed7461
caser1122115 <1729541778@qq.com>
2026-03-14 14:02:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# 1. Force Kernel 6.12
+# 1. Force Kernel 6.12 for Filogic 820 (MT7981)
 sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.12/g' target/linux/mediatek/Makefile
 
-# 2. Set Default IP and Hostname
-sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+# 2. Set Default IP to 192.168.16.1
+sed -i 's/192.168.1.1/192.168.16.1/g' package/base-files/files/bin/config_generate
+
+# 3. Set Hostname to ImmortalWrt-RAX3000M
 sed -i 's/OpenWrt/ImmortalWrt-RAX3000M/g' package/base-files/files/bin/config_generate
 
-# 3. Optimization for MT7981 (RAX3000M Chip)
-# High speed network processing and Wi-Fi drivers fix
+# 4. Remove mac80211 patches to avoid compilation errors on 6.12
 find target/linux/mediatek/patches-6.12 -name "*mac80211*" -exec rm -rf {} \; 2>/dev/null
