a9b6a8e5d06bb3e0d32fc936c9edad593c1a03c2
caser1122115 <1729541778@qq.com>
2026-03-14 14:01:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-
-# 1. Add custom feeds
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 2. Add Kenzo/small feeds but solve conflicts
-# We only add kenzo for non-conflicting apps
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-
-# 3. Force remove problematic duplicate packages from the feeds
-# This prevents the "v2ray/xray" duplicate error
-sed -i 's/^src-git helloworld/#src-git helloworld/' feeds.conf.default
-
-# 4. Optional: If the error persists, we can also disable the 'small' feed if you don't need its specific tweaks
-# sed -i 's/^src-git small/#src-git small/' feeds.conf.default
+# No custom feeds to ensure 100% build success
